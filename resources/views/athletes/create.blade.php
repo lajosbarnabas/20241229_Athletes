7d91b9c3df30883b0f78ba45ce54b28bffa4c574
lajosbarnabas <lajos.barnabas@students.jedlik.eu>
2025-01-01 21:58:45 +0100

    Validation repaired

--- resources/views/athletes/create.blade.php
+++ resources/views/athletes/create.blade.php
@@ -4,28 +4,28 @@
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control @error('name') is-invaled @enderror" id="name" name="name" value="{{old('name')}}">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{old('name')}}">
             @error('name')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="age" class="form-label">Age</label>
-            <input type="text" class="form-control @error('age') is-invaled @enderror" id="age" name="age" value="{{old('age')}}">
+            <input type="text" class="form-control @error('age') is-invalid @enderror" id="age" name="age" value="{{old('age')}}">
             @error('age')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="sport" class="form-label">Sport</label>
-            <input type="text" class="form-control @error('sport') is-invaled @enderror" id="sport" name="sport" value="{{old('sport')}}">
+            <input type="text" class="form-control @error('sport') is-invalid @enderror" id="sport" name="sport" value="{{old('sport')}}">
             @error('sport')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="country" class="form-label">Country</label>
-            <input type="text" class="form-control @error('country') is-invaled @enderror" id="country" name="country" value="{{old('country')}}">
+            <input type="text" class="form-control @error('country') is-invalid @enderror" id="country" name="country" value="{{old('country')}}">
             @error('country')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
--- resources/views/athletes/edit.blade.php
+++ resources/views/athletes/edit.blade.php
@@ -5,28 +5,28 @@
         @method('PUT')
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control @error('name') is-invaled @enderror" id="name" name="name" value="{{ $athlete->name }}">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ $athlete->name }}">
             @error('name')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="age" class="form-label">Age</label>
-            <input type="text" class="form-control @error('age') is-invaled @enderror" id="age" name="age" value="{{ $athlete->age }}">
+            <input type="text" class="form-control @error('age') is-invalid @enderror" id="age" name="age" value="{{ $athlete->age }}">
             @error('age')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="sport" class="form-label">Sport</label>
-            <input type="text" class="form-control @error('sport') is-invaled @enderror" id="sport" name="sport" value="{{ $athlete->sport }}">
+            <input type="text" class="form-control @error('sport') is-invalid @enderror" id="sport" name="sport" value="{{ $athlete->sport }}">
             @error('sport')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="country" class="form-label">Country</label>
-            <input type="text" class="form-control @error('country') is-invaled @enderror" id="country" name="country" value="{{ $athlete->country }}">
+            <input type="text" class="form-control @error('country') is-invalid @enderror" id="country" name="country" value="{{ $athlete->country }}">
             @error('country')
                 <div class="invalid-feedback">{{$message}}</div>
             @enderror
